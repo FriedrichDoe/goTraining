e23c6886a216b7d3cb44da724f30c4ad4a1990e2
Friedrich Kerlach <fkerlach@gmail.com>
2017-06-16 13:44:55 +0200

    functions learned

--- functions/main.go
+++ functions/main.go
@@ -17,7 +17,33 @@ func main() {
 	visit([]int{1, 2, 3, 4}, func(n int) {
 		fmt.Print(n, " ")
 	})
-	fmt.Println()
+	fmt.Println("\n---------------------------")
+
+	xs := filter([]int{1, 2, 3, 4}, func(n int) bool {
+		return n > 1
+	})
+	fmt.Println(xs)
+
+	fmt.Println("---------------------------")
+
+	// anonymous self executed function
+	func() {
+		fmt.Println("anonymous function here!")
+	}()
+
+	// defer change func call order
+	// right before the main exits the defers will be called
+	defer world()
+	hello()
+
+}
+
+func world() {
+	fmt.Println("World")
+}
+
+func hello() {
+	fmt.Print("Hello ")
 }
 
 func greet(fname, lname string, lol int) (string, int) {
@@ -39,4 +65,16 @@ func visit(numbers []int, callback func(int)) {
 	for _, n := range numbers {
 		callback(n)
 	}
+}
+
+func filter(numbers []int, callback func(int) bool) []int {
+	xs := []int{}
+	// or init as var xs []int
+	for _, n := range numbers {
+		if callback(n) {
+			// add the new int var into xs array
+			xs = append(xs, n)
+		}
+	}
+	return xs
 }
